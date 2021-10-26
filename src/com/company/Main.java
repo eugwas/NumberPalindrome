76708247b059ebf6e097738c80198a4a21960caa
Eugeniusz W <eugwas@gmail.com>
2021-10-26 18:18:46 +0200

    Scanner was added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(NumberPalindrome.isPalindrome(-1221));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int number = scanner.nextInt();
+            System.out.println("Is number the palindrome = " + NumberPalindrome.isPalindrome(number));
+        } else {
+            System.out.println("Invalid number");
+        }
+        scanner.nextLine();
+        scanner.close();
     }
 }
